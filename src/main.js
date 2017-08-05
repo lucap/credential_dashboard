19deeab6523ef757dae4cf55c30cb19047972d8c
Luca Pellicoro <luca@hipmunk.com>
2017-08-05 16:56:08 -0700

    functional main menu

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
+const MenuItems = [
+    {id: 'my_credentials', display: 'My Credentials'},
+    {id: 'shared_with_me', display: 'Shared with Me'}
+]
+
 class Dashboard extends React.Component {
 
+    componentWillMount() {
+        // initialize the state with mock data
+        this.setState({
+            data: this.props.data,
+            selectedMenu: 'my_credentials',
+            selectedCredential: null,
+        });
+    }
 
+    onSelectedMenu = (selectedMenu) => {
+        this.setState({
+            selectedMenu
+        });
+    }
 
     render () {
+        console.log(this.state);
+
         return (
             <div className={'Dashboard'}>
-                <MainMenu/>
+                <MainMenu
+                    onSelectedMenu={this.onSelectedMenu}
+                    selectedMenu={this.state.selectedMenu}
+                />
                 <CredentialList/>
                 <CredentialDetails/>
             </div>
@@ -16,15 +39,33 @@ class Dashboard extends React.Component {
 
 class MainMenu extends React.Component {
 
-    render () {
-        return (
-            <div className={'MainMenu'}>
-                <div>My Credentials</div>
-                <div>Shared with Me</div>
-            </div>
+    onClick = (e) => {
+        this.props.onSelectedMenu(
+            e.currentTarget.dataset.id
         );
     }
 
+    render () {
+        return (
+            <div className={'MainMenu'}>
+                {MenuItems.map((item) => {
+                    const classname = this.props.selectedMenu === item.id
+                        ? 'selected'
+                        : null;
+                    return (
+                        <div
+                            key={item.id}
+                            data-id={item.id}
+                            onClick={this.onClick}
+                            className={classname}
+                        >
+                            {item.display}
+                        </div>
+                    );}
+                )}
+            </div>
+        );
+    }
 }
 
 class CredentialList extends React.Component {
@@ -59,7 +100,7 @@ axios.get('/data/initial_data.json')
   .then(function (response) {
     ReactDOM.render(
         <Dashboard
-            data={response}
+            data={response.data}
         />,
         document.getElementById('root')
     );
