8b7e6be37a651a68e27103d2a3fe9250e5836663
Luca Pellicoro <luca@hipmunk.com>
2017-08-05 17:21:12 -0700

    munge data

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ const MenuItems = [
     {id: 'shared_with_me', display: 'Shared with Me'}
 ]
 
+
 class Dashboard extends React.Component {
 
     componentWillMount() {
-        // initialize the state with mock data
         this.setState({
-            data: this.props.data,
+            data: this.props.data, // initialize the state with mock data
             selectedMenu: 'my_credentials',
             selectedCredential: null,
         });
@@ -21,8 +21,6 @@ class Dashboard extends React.Component {
     }
 
     render () {
-        console.log(this.state);
-
         return (
             <div className={'Dashboard'}>
                 <MainMenu
@@ -95,12 +93,29 @@ class CredentialDetails extends React.Component {
 
 }
 
+const mungeData = (data) => {
+    // merge shared_with_others into own_credentials
+    const _data = _.cloneDeep(data);
+    _data.shared_with_others.map( (shared_credential) => {
+        _data.own_credentials.map( (own_credential, index) => {
+            if (own_credential.website === shared_credential.website &&
+                own_credential.username === shared_credential.username
+            ) {
+                own_credential.borrower_user_id = shared_credential.borrower_user_id;
+            }
+        })
+    })
+
+    delete _data.shared_with_others;
+    return _data;
+}
+
 // load the initial data
 axios.get('/data/initial_data.json')
   .then(function (response) {
     ReactDOM.render(
         <Dashboard
-            data={response.data}
+            data={mungeData(response.data)}
         />,
         document.getElementById('root')
     );
